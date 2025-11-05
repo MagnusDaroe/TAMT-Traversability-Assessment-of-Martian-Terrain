7fca95e5d63831013b29124b44336b75da45ab95
Copetags67 <112940124+Copetags67@users.noreply.github.com>
2025-11-05 09:30:50 +0100

    Outline for segmentation cost for Q

---
+++ src/cost_module/src/costmap_segmentation.cpp
@@ -0,0 +1,6 @@
+// Use pointcloud to get frame 2D camera to 3D world
+// Use the transformation from camera frame to global frame
+// mask with labels
+// mask with certainty of labels
+// Risk parameters for different classes
+// Cost function should be C = C_risk * certainty  //C in range [0, 255]
